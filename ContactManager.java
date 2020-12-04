078afbebcbf0da06cb3320a18c74a89bc209e023
Banu500 <75052450+Banu500@users.noreply.github.com>
2020-12-04 14:26:34 -0500

    Add files via upload

--- ContactManager.java
+++ ContactManager.java
@@ -7,21 +7,35 @@ package java_assignment2.pkg2;
 
 /**
  *
- * @author Ibrahim
+ * @author Jeremy , Adam
  */
-    import java.util.Scanner;
-    
-
 public class ContactManager {
     private Contact[] cList;
     private int currentContact,maxcontacts;
     
-  public ContactManager(int max){
+    public ContactManager(int max){
         currentContact=0;
         maxcontacts=max;
         cList = new Contact[max];
     }
-
+    
+    public boolean addContact(String firstName, String lastName, String homePhone, String workPhone, Address homeAddress, String email, String notes, MyDate birthday){
+        if(currentContact<maxcontacts){
+            cList[currentContact]=new Contact(firstName,lastName,homePhone,workPhone,homeAddress,email,notes,birthday); 
+            currentContact++;
+            return true;
+        }
+        return false;
+    }
+    
+    public Contact[] allContacts() {
+        Contact[] s = new Contact[currentContact];
+        for (int i = 0; i < currentContact; i++) {
+            s[i] = cList[i];
+        }
+        return s;
+    }
+    
     public Contact findContact(String firstName, String lastName){
         for (int i = 0; i < currentContact; i++){
             if(cList[i].getFirstName().equals(firstName) && cList[i].getLastName().equals(lastName)) {
@@ -30,13 +44,42 @@ public class ContactManager {
         }
         return null;
     }
-
-     public Contact[] allContacts() {
+    
+    public Contact[] showContact(String firstName, String lastName){
         Contact[] s = new Contact[currentContact];
-        for (int i = 0; i < currentContact; i++) {
-            s[i] = cList[i];
+        for ( int i = 0; i < currentContact; i++){
+            if(cList[i].getFirstName().equals(firstName) && cList[i].getLastName().equals(lastName)) {
+                s[i] = cList[i];
+            }
         }
         return s;
+    }
+    
+    public boolean findCity(String city){
+        Contact[] s = new Contact[currentContact];
+        for ( int i = 0; i < currentContact; i++){
+            if(cList[i].getCity().equals(city)){
+                return true;
+            }
+        }
+        return false;
+    }
+       public String deleteContact(String firstName, String lastName){
+            String s = "";
+            if(findContact(firstName, lastName) == null){
+            s = "Contact not found";
+            return s;
+        }
+            for (int i = 0; i < currentContact; i++){
+            if(cList[i].getFirstName().equals(firstName) && cList[i].getLastName().equals(lastName)){
+            s = "First name " + cList[i].getFirstName() + " last name " + cList[i].getLastName() + " deleted";
+            cList[i] = null;
+            currentContact --;
+            return s;
+    }
+    }
+            s = "Contact deleted";
+            return s;
     }
 
     public Contact[] viewContactsInCity(String city){
@@ -48,44 +91,6 @@ public class ContactManager {
         }
         return s;
     }
-        public Contact[] showContact(String firstName, String lastName){
-        Contact[] s = new Contact[currentContact];
-        for ( int i = 0; i < currentContact; i++){
-            if(cList[i].getFirstName().equals(firstName) && cList[i].getLastName().equals(lastName)) {
-                s[i] = cList[i];
-            }
-        }
-        return s;
-    }
-
-    public boolean findCity(String city){
-        Contact[] s = new Contact[currentContact];
-        for ( int i = 0; i < currentContact; i++){
-            if(cList[i].getCity().equals(city)){
-                return true;
-            }
-        }
-        return false;
-    }
-    public String deleteContact(String firstName, String lastName){
-            String s = "";
-            if(findContact(firstName, lastName) == null){
-            s = "Contact not found";
-            return s;
-        }
-            for (int i = 0; i < currentContact; i++){
-            if(cList[i].getFirstName().equals(firstName) && cList[i].getLastName().equals(lastName)){
-            s = "Contact " + cList[i].getFirstName() + " number " + cList[i].getHomePhone() + " deleted";
-            cList[i] = null;
-            currentContact --;
-            return s;
-    }
-    }
-            s = "Contact deleted";
-            return s;
-    }
 }
-  
-   
-   
+
 
